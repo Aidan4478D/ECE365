12a6e037f76a6dba4e7fb165e6f85092f17d5de5
Aidan Cusa <aidancusa@gmail.com>
2023-09-17 22:21:01 -0400

    completed hash table implementation

--- program1/hash.cpp
+++ program1/hash.cpp
@@ -6,9 +6,11 @@
 using namespace std; 
 
 hashTable::hashTable(int size) {
+    
+    filled = 0; 
 
+    //get prime number
     capacity = getPrime(size);
-    printf("prime number is %d\n", capacity);
     
     //would reserve be a better operation?
     data.resize(capacity);
@@ -16,60 +18,124 @@ hashTable::hashTable(int size) {
 
 int hashTable::insert(const string &key, void *pv) {
     
+    //returns 1 if the key is already in the table
+    if (contains(key))  return 1;
+    
     //if the hash table is >= 50% filled, add more spots to the table
     //if you cant add more spots to the table return an error
     if (((double)filled / capacity) >= 0.5) {
-        if(!rehash())
+        
+        bool increase = rehash(); 
+        //rehash fail check
+        if(!increase)
             return 2; 
     }
-
-    //returns 1 if the key is already in the table
-    if (contains(key)) { return 1; }
     
     //gets hash number index thing based off key
     int hash_val = hash(key); 
-    cout << "key is " << key << " hash val is " << hash_val << "\n"; 
+    int iterations = 0; 
 
     //create new hash item with key
     hashItem *item = new hashItem(); 
     item->key = key; 
-    
+    item->isOccupied = true; 
+    item->isDeleted = false; 
+    item->pv = pv; 
+  
+    while(data[hash_val].isOccupied && !data[hash_val].isDeleted) {
+        
+        //increment index - linear probing
+        hash_val++; 
+
+        //break out of loop if you've iterated through the entire table somehow
+        iterations++; 
+        if(iterations > capacity) return -1; 
+
+        //reset back to zero if hash_val > capacity
+        hash_val %= capacity; 
+    }
+
     //insert item into hash table data
-    data.insert(data.begin() + hash_val, *item); 
+    data[hash_val] = *item; 
     filled++; 
 
     return 0; 
 }
 
 bool hashTable::contains(const string &key) {
-
-    return false; 
+    
+    bool val; 
+    //return true if key is found, false otherwise
+    (findPos(key) == -1) ? val = false : val = true;
+    return val; 
 }
 
 int hashTable::hash(const string &key) {
     
     /* hash function from 
      * Chapter 5.2, Figure 5.4
-     * "Data Structures and Algorithm Analysis in C++", 4th Edition
-by Mark Allen Weiss
-    */
+     * "Data Structures and Algorithm Analysis in C++", 4th Edition by Mark Allen Weis */
 
     unsigned int hashVal = 0;
 
-    for( char ch : key )
+    for (char ch : key)
         hashVal = 37 * hashVal + ch;
 
     return hashVal % capacity;
 }
 
 int hashTable::findPos(const string &key) {
+    
+    int hash_val = hash(key); 
+    int iterations = 0; 
 
-    return 0;
+    while(data[hash_val].isOccupied && !data[hash_val].isDeleted) {
+        if (data[hash_val].key == key) return hash_val; 
+        
+        //increment index - linear probing
+        hash_val++; 
+
+        //break out of loop if you've iterated through the entire table somehow
+        iterations++; 
+        if(iterations > capacity) break; 
+
+        //reset index back to zero if hash_val > capacity
+        hash_val %= capacity; 
+    }
+    
+    //return -1 on failure
+    return -1;
 }
 
 bool hashTable::rehash() {
     
-    return 0; 
+    //double hash capacity
+    capacity = getPrime(capacity * 2); 
+
+    //copy old data into a new vector and clear the current vector
+    vector<hashItem> old_data = data; 
+    data.clear(); 
+
+    //try increasing the capacity size
+    try {
+        data.resize(capacity); 
+    }   
+    //return false if new memory allocation fails
+    catch(bad_alloc&) {
+        return false; 
+    }
+
+    for(int i = 0; i < old_data.size(); i++) {
+
+        //if a hashItem exists in a certain position
+        if(old_data[i].isOccupied && !old_data[i].isDeleted) {
+
+            //will insert item at correct key position in new table
+            insert(old_data[i].key, old_data[i].pv); 
+        }
+    }
+    
+    return true; 
 }
 
 unsigned int hashTable::getPrime(int size) {
@@ -77,14 +143,12 @@ unsigned int hashTable::getPrime(int size) {
     //chose six primes, could add more if more entries are needed
     int primes[NUM_PRIMES] = {98317, 196613, 393241, 786433, 1572869, 3145739};
 
+    //if size is less than 98317, just make table size 98317 anyways
     if (size < primes[0]) { return primes[0]; }
     
     else {
-
-        for(int i = 0; i < NUM_PRIMES-1; i++) {
-            
+        for(int i = 0; i < NUM_PRIMES-1; i++) {  
             if (size > primes[i] && size < primes[i + 1]) {
-               
                 //return bigger prime
                 return primes[i + 1];
             }
