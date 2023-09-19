800b85cdbeb5bbdaed374ddbf282e214c7118150
Aidan Cusa <aidancusa@gmail.com>
2023-09-18 23:30:48 -0400

    fixed bugs in spellcheck logic

--- program1/spellcheck.cpp
+++ program1/spellcheck.cpp
@@ -12,6 +12,17 @@
 
 using namespace std; 
 
+
+//function that takes in a string and converts the entire string to lowercase
+string lowerString(const string &line) {
+    
+    string lower_line = "";  
+    for(auto x : line) {
+        lower_line += tolower(x); 
+    }
+    return lower_line; 
+}
+
 int main() {
   
     string filename;
@@ -20,7 +31,6 @@ int main() {
 
     ifstream dict (filename); 
     string line = ""; 
-    //do something with file name
 
     cout << "Data loaded.\n";
     clock_t t1 = clock(); 
@@ -35,23 +45,20 @@ int main() {
         getline(dict, line);
         if(line.empty()) break;
         
-        string lower_word; 
-        for(auto x : line) {
-            lower_word += tolower(x); 
-        }
+        //convert line to lowercase
+        line = lowerString(line); 
 
         //insert each line in dictionary into hash table
-        hash->insert(lower_word); 
+        hash->insert(line); 
     }
     
-    //do hash table insert and all those checks
     clock_t t2 = clock(); 
     double timeDiff = ((double)(t2-t1)) / CLOCKS_PER_SEC;
 
     cout << "Dictionary load finished. CPU time was " << timeDiff << " seconds\n"; 
-    cout << "Enter name of spell check file: "; 
     
-    //loads new file into inf variable
+    //get spell check file
+    cout << "Enter name of spell check file: "; 
     cin >> filename;
     ifstream check (filename); 
     
@@ -70,37 +77,40 @@ int main() {
         //buffer to store words
         vector<string> buffer; 
         
-        //get content of line from file, if line is empty quit the loop
+        //get content of line from file, if line is empty, skip it
         getline(check, line);
-        
-        //convert word to lowercase
-        string lower_line = "";  
-        for(auto x : line) {
-            lower_line += tolower(x); 
+        if(line.empty()) {
+            line_number++; 
+            continue;
         }
-
+        
+        //convert line to lowercase
+        line = lowerString(line); 
+        
+        //variable to store current word
         string word = "";
         
-        for (int i = 0; i < lower_line.length(); i++) {
+        for (int i = 0; i < line.length(); i++) {
 
             //get letter at position i
-            char x = lower_line[i]; 
-
-            //if the letter is lowercase, an apostrophe, or hyphen, add it onto the word
-            if ((x >= 97 && x <= 122) || (x == 39) || (x == '-')) {
-                word = word + x;
-            }
+            char x = line[i]; 
 
             //if the word is > 20 characters write an error to the output file
-            else if (word.length() >= 20) {
+            if (word.length() >= 20) {
                 ouf << "Long word at line " << line_number << ", starts: " << word << "\n"; 
                 
                 //go to the next word seperator, then set the word to be empty
                 while((x >= 97 && x <= 122) || (x == 39) || (x == '-')) {
-                    x = lower_line[i++]; 
+                    x = line[++i]; 
                 }
                 word = ""; 
             }
+
+            //if the letter is lowercase, an apostrophe, or hyphen, add it onto the word
+            else if ((x >= 97 && x <= 122) || (x == 39) || (x == '-')) {
+                word = word + x;
+            }
+
             //otherwise it's a word seperator and end the word, add it to the buffer
             else {
                 buffer.push_back(word); 
@@ -111,6 +121,11 @@ int main() {
         
         //loop through words that made it onto the buffer and see if they're in the dictionary
         for(int i = 0; i < buffer.size(); i++) {
+            
+            //final purge to get out any empty lines that somehow made it in
+            if (!buffer[i].length()) continue; 
+            
+            //write to output file if the word isn't in the hash table
             if(!hash->contains(buffer[i])) {
                 ouf << "Unknown word at line " << line_number << ": " << buffer[i] << "\n"; 
             }
