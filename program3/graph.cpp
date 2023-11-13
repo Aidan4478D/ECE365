b5b680030c4df60ddc81bc61123b87c280eff3e4
Aidan Cusa <aidancusa@gmail.com>
2023-11-13 01:42:11 -0500

    potentially finished insert function

--- program3/graph.cpp
+++ program3/graph.cpp
@@ -1,3 +1,52 @@
 #include "graph.h"
+#include <limits.h>
 
-//graph stuff
+#define INF INT_MAX
+
+graph::graph(int size) {
+    
+    filled = 0;
+    mapping = hashTable(size); 
+}
+
+void graph::createNode(node* temp_node, string id) {
+    
+    mapping.insert(id, temp_node); 
+
+    temp_node->id = id;
+    temp_node->dist = INF; 
+    temp_node->prev = nullptr; 
+    
+    nodes.push_back(*temp_node); 
+
+    filled++; 
+}
+
+
+void graph::insert(string source, string sink, int cost) {
+
+    node *temp_source; 
+    node *temp_sink;
+
+    //if the starting vertex is already in the graph
+    if(mapping.contains(source)) 
+        temp_source = (node*) mapping.getPointer(source); 
+
+    //otherwise create new vertex in graph
+    else 
+        createNode(temp_source, source); 
+
+    //if the ending vertex is already in the graph
+    if(mapping.contains(sink))
+        temp_source = (node*) mapping.getPointer(sink); 
+
+    //otherwise create new vertex in graph
+    else 
+        createNode(temp_sink, sink); 
+    
+    //jankily adding edge to vector of edges
+    int pos = temp_source->path.size();
+    temp_source->path.resize(++pos); 
+    temp_source->path[pos].cost = cost; 
+    temp_source->path[pos].sink = temp_sink; 
+}
--- program3/graph.h
+++ program3/graph.h
@@ -12,20 +12,31 @@ using namespace std;
 class graph {
 
     class node { // An inner class within heap
+
+        class edge {
+            
+            public:
+                node *sink; 
+                int cost; 
+        }; 
+
         public:
             std::string id; // The id of this node
-            
-            //adjacency list with edge costs
-            list<tuple<node, int>> adjacent;
-            
-            //log of verticies along a path
-            list<string> path; 
+            int dist; //cost of the best path found so far from source->vertex
+            node *prev; //previous node on the path from source->vertex
+
+            //edges connected to the vertex
+            vector<edge> path; 
 
             node() = default; 
     }; 
+    
+    void createNode(node* temp_node, string id); 
 
     list<node> nodes; 
     hashTable mapping; 
+
+    int capacity, filled; 
         
     public:
         graph(int size); 
